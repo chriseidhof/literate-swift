b65f514969dbfd6baa33a15bcc89e87787a1f96e
Chris Eidhof <chris@eidhof.nl>
2015-07-17 14:07:15 +0200

    Initial Commit

---
+++ LiterateSwift/LiterateSwift.h
@@ -0,0 +1,19 @@
+//
+//  LiterateSwift.h
+//  LiterateSwift
+//
+//  Created by Chris Eidhof on 17/07/15.
+//  Copyright © 2015 Chris Eidhof. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LiterateSwift.
+FOUNDATION_EXPORT double LiterateSwiftVersionNumber;
+
+//! Project version string for LiterateSwift.
+FOUNDATION_EXPORT const unsigned char LiterateSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiterateSwift/PublicHeader.h>
+
+
